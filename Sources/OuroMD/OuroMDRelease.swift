e14777ff2e9155eb5217770d469befacd040bd9c
Ari Mendelow <ari@mendelow.me>
2026-06-14 16:01:37 -0700

    feat(updater): add Unit 1b pure update logic

---
+++ Sources/OuroMD/OuroMDRelease.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum OuroMDRelease {
+    static let appName = "Ouro MD"
+    static let bundleIdentifier = "org.ourostack.ouro-md"
+    static let repository = "ourostack/ouro-md"
+    static let version = "0.9.0"
+    static let userAgent = "OuroMD/\(version)"
+}
